8dc943ce66cc229bcc883248acd64762b8516004
JoshyDerEchte <joshua.schlautmann@gmail.com>
2022-09-15 15:30:29 +0200

    first working Stages with some bugs co commited my Niklas

--- TikTakToe/script.js
+++ TikTakToe/script.js
@@ -1,5 +1,5 @@
-const player_x_class ='x'
-const player_o_class ='circle'
+const player_x_class ='x';
+const player_o_class ='circle';
 const winning_combinations = [
     [0, 1 ,2],
     [3, 4, 5],
@@ -18,28 +18,74 @@ const restartButton = document.getElementById('restartButton');
 const winningMessageTextElement = document.getElementById('winningMessageText');
 let isPlayer_O_Turn = false;
 
-startGame ()
+startGame ();
+
+restartButton.addEventListener('click', startGame);
 
 function startGame (){ // Löschen aller Eingaben von vorherigen Spielen 
+    isPlayer_O_Turn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove(player_x_class);
+        cell.classList.remove(player_o_class);
+        cell.removeEventListener('click', handleCellClick);
+        cell.addEventListener('click', handleCellClick, {once: true});
+    })
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
 
 }
 
 function handleCellClick(e) { // Checken welcher Spieler dran ist 
-                                // Aufrufen von placeMark, if Schleife um zu checken ob ein Spieler gewonnen hat / unentschieden gespielt wurde, ansonsten aufrufen von swapTurns()
+    const cell = e.target;
+    const currentClass = isPlayer_O_Turn ? player_o_class : player_x_class;      // Aufrufen von placeMark, if Schleife um zu checken ob ein Spieler gewonnen hat / unentschieden gespielt wurde, ansonsten aufrufen von swapTurns()
+    placeMark (cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame (false);
+    } else if (isDraw()){
+        endGame (true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
 }
 
-function endGame (){        // If Schleife ob Unentschieden oder der Spieler welcher dran ist gewonnen hat. Anzeigen wie es ausgegangen ist
-
+function endGame (draw){        // If Schleife ob Unentschieden oder der Spieler welcher dran ist gewonnen hat. Anzeigen wie es ausgegangen ist
+    if (draw){
+        winningMessageTextElement.innerText = 'Unentschieden';
+    } else {
+        winningMessageTextElement.innerText = `Spieler mit ${isPlayer_O_Turn ? "O" : "X"} hat gewonnen`;
+    }
+    winningMessageElement.classList.add('show');
 }
 
 function isDraw (){ // 
-
+    return [...cellElements].every(cell =>{
+        return cell.classList.contains(player_x_class) || cell.classList.contains(player_o_class);
+    })
 }
 
-function placeMark(){ // auf einer Zelle ein Zeichen des aktiven Spielers einfügen
-
+function placeMark(cell, currentClass){ // auf einer Zelle ein Zeichen des aktiven Spielers einfügen
+    cell.classList.add(currentClass);
 }
     
 function swapTurns(){ // isPlayer_0_Turn boolean switchen
     isPlayer_O_Turn = !isPlayer_O_Turn;
+}
+
+function setBoardHoverClass(){
+    boardElement.classList.remove(player_x_class);
+    boardElement.classList.remove(player_o_class);
+    if (isPlayer_O_Turn){
+        boardElement.classList.add(player_o_class);
+    } else {
+        boardElement.classList.add(player_x_class);
+    }
+}
+
+function checkWin(currentClass){
+    return winning_combinations.some(combination =>{
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        })
+    })
 }
