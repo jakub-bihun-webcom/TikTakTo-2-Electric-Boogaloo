950ff39fc0c3fd8eb211037101c75f5803c51786
Niklas Kiefer <niklas.kiefer@web-computing.de>
2022-09-15 10:52:19 +0200

    added Functions

--- TikTakToe/script.js
+++ TikTakToe/script.js
@@ -9,7 +9,38 @@ const winning_combinations = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-/*  012
-    345
-    678    */
 ]
+
+const cellElements = document.querySelectorAll('[data-cell]');
+const boardElement = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.getElementById('winningMessageText');
+let isPlayer_O_Turn = false;
+
+startGame ()
+
+function startGame (){ // Löschen aller Eingaben von vorherigen Spielen 
+
+}
+
+function handleCellClick(e) { // Checken welcher Spieler dran ist 
+                                // Aufrufen von placeMark, if Schleife um zu checken ob ein Spieler gewonnen hat / unentschieden gespielt wurde, ansonsten aufrufen von swapTurns()
+}
+
+function endGame (){        // If Schleife ob Unentschieden oder der Spieler welcher dran ist gewonnen hat. Anzeigen wie es ausgegangen ist
+
+}
+
+function isDraw (){ // 
+
+}
+
+function placeMark(){ // auf einer Zelle ein Zeichen des aktiven Spielers einfügen
+
+}
+    
+function swapTurns(){ // isPlayer_0_Turn boolean switchen
+    isPlayer_O_Turn = !isPlayer_O_Turn;
+}
+
