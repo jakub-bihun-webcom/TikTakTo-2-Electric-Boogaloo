b8cd7c95d28d108955647847f1a5faa3d92814c5
Niklas Kiefer <niklas.kiefer@web-computing.de>
2022-10-13 09:19:37 +0200

    Design UI

--- TicTacToeCombined/scripts/confetti.js
+++ TicTacToeCombined/scripts/confetti.js
@@ -215,7 +215,7 @@ var confetti = {
 const start = () => {
   setTimeout(function() {
       confetti.start()
-  }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+  }, 1); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
 }
 
 const stop = () => {
