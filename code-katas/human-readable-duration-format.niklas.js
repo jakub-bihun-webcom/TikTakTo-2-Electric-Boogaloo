402d5fc76de09e77eae6244bdae06ca7fea6c6a3
Niklas Kiefer <niklas.kiefer@web-computing.de>
2022-10-17 17:02:19 +0200

    Add Comments

--- code-katas/human-readable-duration-format.niklas.js
+++ code-katas/human-readable-duration-format.niklas.js
@@ -7,6 +7,11 @@ function formatDurationNiklas(seconds) {
     }
 }
 
+/**
+ * Funktion zum ausrechnen der Zeiten.
+ * @param {number} time 
+ * @returns 
+ */
 function calculateDuration(time) {
     const timeArray = [];
     const years = Math.floor(time / 31536000);
@@ -18,14 +23,6 @@ function calculateDuration(time) {
     const minutes = Math.floor(restOfHours / 60);
     const seconds = restOfHours % 60;
     timeArray.push(years, days, hours, minutes, seconds);
-
-    console.log('TimeArray', timeArray);
-    console.log(output(years, 'year')
-    + commaDays(timeArray) + output(days, 'day')
-    + commaHours(timeArray) + output(hours, 'hour')
-    + commaMinutes(timeArray) + output(minutes, 'minute')
-    + commaSeconds(timeArray) + output(seconds, 'second'));
-
     return output(years, 'year')
             + commaDays(timeArray) + output(days, 'day')
             + commaHours(timeArray) + output(hours, 'hour')
@@ -33,6 +30,12 @@ function calculateDuration(time) {
             + commaSeconds(timeArray) + output(seconds, 'second');
 }
 
+/**
+ * Überprüft ob der Input ungleich 0 ist und fügt bei dem text ein 's' für die Mehrzahl ein. 
+ * @param {[number]} input 
+ * @param {string} text 
+ * @returns 
+ */
 function output(input, text) {
     if (input === 0) {
         return '';
@@ -41,6 +44,11 @@ function output(input, text) {
     } else return input + ' ' + text + 's'
 }
 
+/**
+ * Funktion, welche überprüft ob an dieser Stelle ein Komma ein 'and' oder nichts eingefügt werden soll.
+ * @param {[number]} timeArray 
+ * @returns 
+ */
 function commaDays(timeArray) {
     const newArray = [timeArray[1], timeArray[2], timeArray[3], timeArray[4]];
     if (timeArray[0] === 0) {
@@ -50,6 +58,11 @@ function commaDays(timeArray) {
     } else return ', '
 }
 
+/**
+ * Funktion, welche überprüft ob an dieser Stelle ein Komma ein 'and' oder nichts eingefügt werden soll.
+ * @param {[number]} timeArray 
+ * @returns 
+ */
 function commaHours(timeArray) {
     const newArray = [timeArray[2], timeArray[3], timeArray[4]];
     if (timeArray[0] === 0 && timeArray[1] === 0) {
@@ -57,10 +70,15 @@ function commaHours(timeArray) {
     } else if (timeArray[4] === 0 && timeArray[3] === 0 && timeArray[2] === 0) {
         return ''
     } else if (newArray.filter(biggerThan1).length === 1) {
-        return ' and '
+        return 'and '
     } else return ', '
 }
 
+/**
+ * Funktion, welche überprüft ob an dieser Stelle ein Komma ein 'and' oder nichts eingefügt werden soll.
+ * @param {[number]} timeArray 
+ * @returns 
+ */
 function commaMinutes(timeArray) {
     const newArray = [timeArray[3], timeArray[4]]
     if (timeArray[0] === 0 && timeArray[1] === 0 && timeArray[2] === 0) {
@@ -75,18 +93,25 @@ function commaMinutes(timeArray) {
     else return ', '
 }
 
+/**
+ * Funktion, welche überprüft ob an dieser Stelle ein Komma ein 'and' oder nichts eingefügt werden soll.
+ * @param {[number]} timeArray 
+ * @returns 
+ */
 function commaSeconds(timeArray) {
     const newArray = [timeArray[0], timeArray[1], timeArray[2], timeArray[3]]
     if (timeArray[4] === 0) {
         return ''
     } else if (newArray.filter(biggerThan1).length === 0) {
         return ''
-    } else if (timeArray[3] === 0){
-        return ''
-    }
-        return ' and '
+    } else return ' and '
 }
 
+/**
+ * Funktion zum filtern der Arrays, hier werden alle nullen aus den Arrays gefiltert. 
+ * @param {number} number 
+ * @returns 
+ */
 function biggerThan1(number) {
     return number > 0;
 }
