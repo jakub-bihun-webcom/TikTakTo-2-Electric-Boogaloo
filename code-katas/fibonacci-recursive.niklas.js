7a9a5d5c554758f7aceddb7ab04c26ffa7d2c648
Niklas Kiefer <niklas.kiefer@web-computing.de>
2022-10-26 17:06:06 +0200

    Format Changes

--- code-katas/fibonacci-recursive.niklas.js
+++ code-katas/fibonacci-recursive.niklas.js
@@ -2,19 +2,18 @@ let fibMap = new Map();
 
 /**
  * Funktion zum Berechnen der nten Fibonacci Zahl
- * @param {number} steps 
+ * @param {number} steps
  * @returns {number} Fibonacci Zahl
  */
 function fibonacciRecursive(steps) {
-    if (steps === 0) {
-      return 0;
-    }
-    if (steps < 3) {
-      return 1;
-    }
-    if (!fibArr.get(steps)){
-      fibArr.set(steps, fibonacciRecursive(steps - 1) + fibonacciRecursive(steps - 2))  
-    }
-    return fibArr.get(steps);
+  if (steps === 0) {
+    return 0;
   }
-  
+  if (steps < 3) {
+    return 1;
+  }
+  if (!fibArr.get(steps)) {
+    fibArr.set(steps,fibonacciRecursive(steps - 1) + fibonacciRecursive(steps - 2));
+  }
+  return fibArr.get(steps);
+}
