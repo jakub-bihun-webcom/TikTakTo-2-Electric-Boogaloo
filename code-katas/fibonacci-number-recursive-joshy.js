4c42b061790c849aba6cd0382cdc36c46405cf74
JoshyDerEchte <joshua.schlautmann@gmail.com>
2022-11-02 11:31:47 +0100

    Add recursive solution for fibonacci formula, Use Objekt as Memory,

---
+++ code-katas/fibonacci-number-recursive-joshy.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} steps Die Stelle in der fibonacci Formel 
+ * @param {number} value Der Wert für die Stelle in der fibonacci Formel
+ * @returns {number} Die Fibonacci Zahl an der stelle "steps"
+ * Konvertiert die Eingabe in die zutreffende Fibonacci Zahl.
+ * Das ganze passiert rekursiv.
+ */
+const fibonacciRecursive = (steps, value = {}) => {
+    if (steps === 0) {
+        return 0;
+    } else if (steps <= 2) {
+        return 1;
+    }
+
+    if (value[steps]) {
+        return value[steps];
+    }
+
+    value[steps] = fibonacciRecursive(steps - 1, value) + fibonacciRecursive(steps - 2, value);
+    return value[steps];
+};
--- code-katas/fibonacci-number.spec.js
+++ code-katas/fibonacci-number.spec.js
@@ -1,17 +1,17 @@
-describe("Fibonacci Number should be", () => {
-    const executeTests = (functionToTest) => {
+describe('Fibonacci Number should be', () => {
+    const executeTests = functionToTest => {
         expect(functionToTest(17)).toBe(1597);
-        //expect(functionToTest(20)).toBe(6765);
-        //expect(functionToTest(0)).toBe(0);
-        //expect(functionToTest(4)).toBe(3);
-        //expect(functionToTest(1)).toBe(1);
-    }
+        expect(functionToTest(20)).toBe(6765);
+        expect(functionToTest(0)).toBe(0);
+        expect(functionToTest(4)).toBe(3);
+        expect(functionToTest(1)).toBe(1);
+    };
 
-    it("Niklas", () => {
+    it('Niklas', () => {
         executeTests(fibonacciNiklas);
     });
-    it("Joshua", () => {
-        executeTests(fibJoshua);
-    });
 
+    it('Joshua', () => {
+        executeTests(fibonacciRecursive);
+    });
 });
