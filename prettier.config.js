e7c5216037602dd4e76b057cb7787db3c9d8aca5
Niklas Kiefer <niklas.kiefer@web-computing.de>
2022-11-16 14:50:07 +0100

    Change config

--- prettier.config.js
+++ prettier.config.js
@@ -2,8 +2,6 @@ module.exports = {
     arrowParens: 'avoid', // default: "es5"
     printWidth: 120, // default: 80
     singleQuote: true, // default: false
-    tabWidth: 4, // default: 2
     trailingComma: 'none', // default: "es5",
     bracketSameLine: true, // default: false
-    endOfLine: 'auto' // default: 'lf'
 };
