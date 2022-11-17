0651a32b8d9a4a1aa2ffd11622a57a424f6a4ee0
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2022-11-17 17:40:52 +0100

    Add Tests

---
+++ code-katas/car-mileage/car-mileage.spec.js
@@ -0,0 +1,15 @@
+describe('Catching Car Mileage Numbers:', () => {
+  const executeTests = functionToTest => {
+    expect(functionToTest(3, [1337, 256])).toBe(0);
+    expect(functionToTest(1336, [1337, 256])).toBe(1);
+    expect(functionToTest(1337, [1337, 256])).toBe(2);
+    expect(functionToTest(11208, [1337, 256])).toBe(0);
+    expect(functionToTest(11209, [1337, 256])).toBe(1);
+    expect(functionToTest(11211, [1337, 256])).toBe(2);
+
+  };
+
+  it('Joshua', () => {
+    executeTests(isInteresting);
+  });
+});
