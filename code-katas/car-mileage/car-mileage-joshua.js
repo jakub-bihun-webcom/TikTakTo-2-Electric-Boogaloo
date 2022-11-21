ec12844e17d9920280716bc6548fc4c5b5a71eec
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2022-11-21 14:49:35 +0100

    fix readability

--- code-katas/car-mileage/car-mileage-joshua.js
+++ code-katas/car-mileage/car-mileage-joshua.js
@@ -1,6 +1,6 @@
 /**
- * @param number
- * @param awesomePhrase
+ * @param {number} number
+ * @param {number[]} awesomePhrase
  * @returns {number}
  */
 function isInteresting(number, awesomePhrase) {
@@ -10,48 +10,50 @@ function isInteresting(number, awesomePhrase) {
 const carMileage = {
   /**
    * Checkt, ob eine gegebene Zahl interessant ist oder nicht
-   * @param number
-   * @param awesomePhrase
+   * @param {number} number
+   * @param {number[]} awesomePhrase
    * @returns {number}
    */
   isInteresting: function (number, awesomePhrase) {
     const numberPlusOne = number + 1;
     const numberPlusTwo = number + 2;
-    const awesomeNumber = this._numberIsAwesome(number, awesomePhrase);
-    const awesomeNumberPlusOne = this._numberIsAwesome(numberPlusOne, awesomePhrase);
-    const awesomeNumberPlusTwo = this._numberIsAwesome(numberPlusTwo, awesomePhrase);
+    const numberIsAwesome = this._numberIsAwesome(number, awesomePhrase);
+    const numberPlusOneIsAwesome = this._numberIsAwesome(numberPlusOne, awesomePhrase);
+    const numberPlusTwoIsAwesome = this._numberIsAwesome(numberPlusTwo, awesomePhrase);
 
-    if (awesomeNumber) {
+    if (numberIsAwesome) {
       return 2;
-    } else if (awesomeNumberPlusOne || awesomeNumberPlusTwo) {
-      return 1;
-    } else {
-      return 0;
     }
+    if (numberPlusOneIsAwesome || numberPlusTwoIsAwesome) {
+      return 1;
+    }
+      return 0;
   },
 
   /**
-   * @param number
-   * @param awesomePhrase
-   * @returns {boolean|number}
+   * @param {number} number
+   * @param {number[]} awesomePhrase
+   * @returns {boolean}
    * @private
    */
   _numberIsAwesome: function (number, awesomePhrase) {
     if (number < 100) {
-      return 0;
+      return false;
     }
+
     const zeros = this._checkZeros(number);
     const sameNumbers = this._checkSameNumbers(number);
     const countingUp = this._checkCountingUp(number);
     const countingDown = this._checkCountingDown(number);
     const palindrome = this._checkPalindrome(number);
     const isAwesomePhrase = this._checkAwesomePhrase(number, awesomePhrase);
+
     return zeros || sameNumbers || countingUp || countingDown || palindrome || isAwesomePhrase;
   },
 
   /**
-   * @param number
-   * @returns {this is number[]}
+   * @param {number} number
+   * @returns {boolean}
    * @private
    */
   _checkZeros: function (number) {
@@ -61,8 +63,9 @@ const carMileage = {
   },
 
   /**
-   * @param number
-   * @returns {this is number[]}
+   *
+   * @param {number} number
+   * @returns {boolean}
    * @private
    */
   _checkSameNumbers: function (number) {
@@ -71,65 +74,71 @@ const carMileage = {
   },
 
   /**
-   * @param number
+   * @param {number} number
    * @returns {boolean}
    * @private
    */
   _checkCountingUp: function (number) {
     const numberArray = Array.from(String(number), Number);
-    const transformedArray = [];
-    for (let i = 0; i < numberArray.length; i++) {
-      if (numberArray[i] === 0) {
-        transformedArray.push(10);
+
+    const transformedArray = numberArray.map(number => {
+      if (number === 0) {
+        return 10;
       } else {
-        transformedArray.push(numberArray[i]);
+        return number;
       }
-    }
-    let passedCheck = false;
+    });
+
     for (let i = 0; i < transformedArray.length - 1; i++) {
       if (transformedArray[i] + 1 !== transformedArray[i + 1]) {
-        passedCheck = true;
+        return false;
       }
     }
-    return !passedCheck;
+
+    return true;
   },
 
   /**
-   * @param number
+   * @param {number} number
    * @returns {boolean}
    * @private
    */
   _checkCountingDown: function (number) {
     const numberArray = Array.from(String(number), Number);
     let passedCheck = false;
+
     for (let i = 0; i < numberArray.length - 1; i++) {
       if (numberArray[i] - 1 !== numberArray[i + 1]) {
         passedCheck = true;
       }
     }
+
     return !passedCheck;
   },
 
   /**
-   * @param number
+   * @param {number} number
    * @returns {boolean}
    * @private
    */
   _checkPalindrome: function (number) {
     const numberArray = Array.from(String(number), Number);
     const reverseArray = [];
+
     for (let i = numberArray.length - 1; i >= 0; i--) {
       reverseArray.push(numberArray[i]);
     }
+
     const reverseArrayToString = reverseArray.toString();
     const numberArrayToString = numberArray.toString();
+
     return reverseArrayToString === numberArrayToString;
   },
 
   /**
-   * @param number
+   * @param {number} number
    * @param awesomePhrase
-   * @returns {*}
+   * @returns {boolean}
    * @private
    */
   _checkAwesomePhrase: function (number, awesomePhrase) {
