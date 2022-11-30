2246540deaca9f1fefd3a8540118c4b9fda3566f
Stefan Becking <sb@web-computing.de>
2022-11-30 14:57:47 +0100

    Add package.json and solution for AoC 2021 day 1

---
+++ advent-of-code/2021/day1-sonar-sweep/day1-stefan.js
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+
+const inputFile = fs.readFileSync('input.txt', { encoding: 'utf8' });
+const input = inputFile
+  .toString()
+  .trim()
+  .split('\n')
+  .map(num => parseInt(num, 10));
+const solution = solve(input);
+console.log(solution);
+
+/**
+ * @param {number[]} measurements
+ * @return {number}
+ */
+function solve(measurements) {
+  let previousMeasurement;
+  let increaseCount = 0;
+
+  console.log(measurements);
+
+  measurements.forEach(measurement => {
+    if (previousMeasurement !== undefined && measurement > previousMeasurement) {
+      increaseCount++;
+    }
+    previousMeasurement = measurement;
+  });
+
+  return increaseCount;
+}
