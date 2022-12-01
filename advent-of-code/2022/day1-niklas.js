f8c39049932a0c76c3a13ae3f653cab0a894c912
Niklas Kiefer <niklas.kiefer@web-computing.de>
2022-12-01 09:33:48 +0100

    Run Prettier

--- advent-of-code/2022/day1-niklas.js
+++ advent-of-code/2022/day1-niklas.js
@@ -2,31 +2,29 @@ import * as fs from 'fs';
 
 const inputFile = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const input = inputFile
-        .toString()
-        .trim()
-        .split('\n')
-        .map(num => parseInt(num, 10));
+  .toString()
+  .trim()
+  .split('\n')
+  .map(num => parseInt(num, 10));
 const solution = maxCalories(input);
 
-function maxCalories(input){
-    console.log(input.length)
-    let currentHighest = 0;
-    let currentCalories = 0;
-    for (let i = 0; i < input.length; i++) {
-        if (!isNaN(input[i])) {
-            currentCalories = input[i] + currentCalories;
-        }
-
-        if (isNaN(input[i])){
-            return
-        }
+function maxCalories(input) {
+  console.log(input.length);
+  let currentHighest = 0;
+  let currentCalories = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (!isNaN(input[i])) {
+      currentCalories = input[i] + currentCalories;
     }
+    if (currentCalories > currentHighest) {
+      currentHighest = currentCalories;
+    }
+    if (isNaN(input[i])) {
+      return;
+    }
+  }
 
-
-
-    return currentHighest
+  return currentHighest;
 }
 
-console.log(solution)
-
-
+console.log(solution);
