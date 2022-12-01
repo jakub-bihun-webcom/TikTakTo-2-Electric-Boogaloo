2c8ebfacd345c698817616a47b918d7ee95c1f51
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2022-12-01 18:18:48 +0100

    mittlerweile ist es nicht mehr aus Versehen xD

--- advent-of-code/2022/transferInput.js
+++ advent-of-code/2022/transferInput.js
@@ -1,14 +1,13 @@
 import * as fs from 'fs';
 
 export function inputToObject() {
-  const inputFile = fs.readFileSync('day1-Calorie-Counting/input-joshua.txt', { encoding: 'utf8' });
+  const inputFile = fs.readFileSync('input.txt', { encoding: 'utf8' });
   const input = inputFile.toString().trim().split('\n');
   return input;
 }
 
 export function inputToString() {
-  const inputFile = fs.readFileSync('day1-Calorie-Counting/input-joshua.txt', { encoding: 'utf8' });
+  const inputFile = fs.readFileSync('input.txt', { encoding: 'utf8' });
   const input = inputFile.toString().replace(/\n/g, ' ').replace(/  /g, ' ');
   return input;
 }
-console.log(inputToString());
