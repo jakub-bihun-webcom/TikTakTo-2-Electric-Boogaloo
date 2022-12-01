939c1518d6e694171184cc3ecb177deb9f1cdc6d
Niklas Kiefer <niklas.kiefer@web-computing.de>
2022-12-01 09:58:40 +0100

    Add Solution

--- advent-of-code/2022/day1-Calorie-Counting/day1-niklas.js
+++ advent-of-code/2022/day1-Calorie-Counting/day1-niklas.js
@@ -8,22 +8,26 @@ const input = inputFile
   .map(num => parseInt(num, 10));
 const solution = maxCalories(input);
 
+/**
+ *
+ * @param input
+ * @returns {number}
+ */
 function maxCalories(input) {
   console.log(input.length);
   let currentHighest = 0;
   let currentCalories = 0;
+
   for (let i = 0; i < input.length; i++) {
     if (!isNaN(input[i])) {
       currentCalories = input[i] + currentCalories;
-    }
-    if (currentCalories > currentHighest) {
-      currentHighest = currentCalories;
-    }
-    if (isNaN(input[i])) {
-      return;
+      if (currentCalories > currentHighest) {
+        currentHighest = currentCalories;
+      }
+    } else if (isNaN(input[i])) {
+      currentCalories = 0;
     }
   }
-
   return currentHighest;
 }
 
