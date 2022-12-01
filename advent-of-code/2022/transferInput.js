943da62b82d023529d94321dbfa216ef077c4bdc
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2022-12-01 18:17:57 +0100

    rename and add inputToString

--- advent-of-code/2022/transferInput.js
+++ advent-of-code/2022/transferInput.js
@@ -1,18 +1,14 @@
 import * as fs from 'fs';
 
-export function inputToArray () {
-    const inputFile = fs.readFileSync('day1-Calorie-Counting/input-joshua.txt', { encoding: 'utf8' });
-    const input = inputFile.toString().trim().split('\n');
-    return input
+export function inputToObject() {
+  const inputFile = fs.readFileSync('day1-Calorie-Counting/input-joshua.txt', { encoding: 'utf8' });
+  const input = inputFile.toString().trim().split('\n');
+  return input;
 }
 
-export function inputToString () {
-    const inputFile = fs.readFileSync('day1-Calorie-Counting/input-joshua.txt', { encoding: 'utf8' });
-    const input = inputFile
-        .toString()
-        .trim()
-        .split('\n')
-        .map(num => parseInt(num, 10));
-    return input
+export function inputToString() {
+  const inputFile = fs.readFileSync('day1-Calorie-Counting/input-joshua.txt', { encoding: 'utf8' });
+  const input = inputFile.toString().replace(/\n/g, ' ').replace(/  /g, ' ');
+  return input;
 }
-console.log(typeof inputToArray())
+console.log(inputToString());
