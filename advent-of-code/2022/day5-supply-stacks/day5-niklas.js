0dad9b2988e04cc784988651f3182e577e3a447e
Niklas Kiefer <niklas.kiefer@web-computing.de>
2022-12-07 17:15:46 +0100

    Aufgabe 5

--- advent-of-code/2022/day5-supply-stacks/day5-niklas.js
+++ advent-of-code/2022/day5-supply-stacks/day5-niklas.js
@@ -29,8 +29,8 @@ function createOrder(line) {
 
   const start = crateOrder[startRow - 1];
   const finnish = crateOrder[finRow - 1];
-
-  moveCrate(amount, start, finnish, startRow, finRow);
+  moveCrate2(amount, start, finnish, startRow, finRow);
+  // moveCrate(amount, start, finnish, startRow, finRow);
 }
 
 function moveCrate(amount, start, finnish, startRow, finRow) {
@@ -39,16 +39,19 @@ function moveCrate(amount, start, finnish, startRow, finRow) {
   }
   crateOrder[startRow - 1] = start;
   crateOrder[finRow - 1] = finnish;
+}
 
-  console.log('Row1: ' + crateOrder[0]);
-  console.log('Row2: ' + crateOrder[1]);
-  console.log('Row3: ' + crateOrder[2]);
-  console.log('Row4: ' + crateOrder[3]);
-  console.log('Row5: ' + crateOrder[4]);
-  console.log('Row6: ' + crateOrder[5]);
-  console.log('Row7: ' + crateOrder[6]);
-  console.log('Row8: ' + crateOrder[7]);
-  console.log('Row9: ' + crateOrder[8]);
+function moveCrate2(amount, start, finnish, startRow, finRow) {
+  let temp = [];
+  temp = start.slice(start.length - amount);
+  for (let i = 0; i < amount; i++) {
+    start.pop()
+  }
+  console.log("start " + start)
+  console.log("finnish: " + finnish)
+  const finnish2 = finnish.concat(temp);
+  crateOrder[startRow - 1] = start;
+  crateOrder[finRow - 1] = finnish2;
 }
 
 function numbersInArray(number) {
@@ -63,4 +66,15 @@ function numbersInArray(number) {
   }
 }
 
+function printOutput() {
+  console.log('Row1: ' + crateOrder[0]);
+  console.log('Row2: ' + crateOrder[1]);
+  console.log('Row3: ' + crateOrder[2]);
+  console.log('Row4: ' + crateOrder[3]);
+  console.log('Row5: ' + crateOrder[4]);
+  console.log('Row6: ' + crateOrder[5]);
+  console.log('Row7: ' + crateOrder[6]);
+  console.log('Row8: ' + crateOrder[7]);
+  console.log('Row9: ' + crateOrder[8]);
+}
 getAllNumbers();
