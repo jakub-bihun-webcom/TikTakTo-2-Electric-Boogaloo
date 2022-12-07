90282f4bc623bd3bf4381a94f78774bed450aef6
Niklas Kiefer <niklas.kiefer@web-computing.de>
2022-12-07 14:29:05 +0100

    Add Files for Aufgabe5

---
+++ advent-of-code/2022/day5-suppy-stacks/day5-niklas.js
@@ -0,0 +1,35 @@
+import { inputFileReader } from '../input-file-reader.js';
+const inputLines = inputFileReader.readAsStringArray('input-niklas.txt');
+
+const crateOrder = [
+  ['T', 'D', 'W', 'Z', 'V', 'P'],
+  ['L', 'S', 'W', 'V', 'F', 'J', 'D'],
+  ['Z', 'M', 'L', 'S', 'V', 'T', 'B', 'H'],
+  ['R', 'S', 'J'],
+  ['C', 'Z', 'B', 'G', 'F', 'M', 'L', 'W'],
+  ['Q', 'W', 'V', 'H', 'Z', 'R', 'G', 'B'],
+  ['V', 'J', 'P', 'C', 'B', 'D', 'N'],
+  ['P', 'T', 'B', 'Q'],
+  ['H', 'G', 'Z', 'R', 'C']
+];
+
+function getAllNumber() {
+  inputLines.forEach(numbers => {
+    const replaced = numbers.replace(/\D/g, '');
+    numbersInArray(replaced);
+  });
+}
+
+function numbersInArray(number) {
+  if (number.length === 3) {
+    return Array.from(String(number), Number);
+  } else {
+    const arr = [];
+    arr.push(parseInt(number[0] + number[1]));
+    arr.push(parseInt(number[2]));
+    arr.push(parseInt(number[3]));
+    console.log(arr);
+  }
+}
+
+getAllNumber();
