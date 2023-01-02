4f60002ce28c21dd80b83b15596df3e911c8e918
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2023-01-02 18:45:08 +0100

    First Changes

--- pokedex-API/pokedex/src/main.ts
+++ pokedex-API/pokedex/src/main.ts
@@ -1,23 +1,16 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import './style.css';
+import { setupCounter } from './counter';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <a href="https://www.pokemon.com/de" target="_blank">
+      <img src="/International_Pokémon_logo.svg.png" class="logo" alt="Pokemon logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Pokedex</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
