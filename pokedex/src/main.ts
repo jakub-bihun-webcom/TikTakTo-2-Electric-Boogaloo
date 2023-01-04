bed093d151908be547ec4cfaed9c09d6d7849aff
Antonio Sarcevic <antonio.sarcevic@web-computing.de>
2023-01-04 08:57:28 +0100

    Recommend `es6-string-html`

--- pokedex/src/main.ts
+++ pokedex/src/main.ts
@@ -2,7 +2,7 @@ import '@picocss/pico/css/pico.min.css';
 import './style.css';
 import { setupCounter } from './counter';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
   <main class="container">
     <a href="https://www.pokemon.com/de" target="_blank">
       <img src="/International_Pokémon_logo.svg.png" class="logo" alt="Pokemon logo" />
