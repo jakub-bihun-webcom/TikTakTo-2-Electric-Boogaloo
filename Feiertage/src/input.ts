a70dd36b5d8d15f897e3c68a103c7237a102ff4e
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-01-11 16:26:03 +0100

    Change Table + add Pico

--- Feiertage/src/input.ts
+++ Feiertage/src/input.ts
@@ -3,12 +3,8 @@ export async function getFeiertage() {
   let bundeslandValue = bundeslandInput.value;
   console.log(bundeslandValue);
 
-  let output = document.getElementById('msg');
-
   const response = await fetch(`https://feiertage-api.de/api/?jahr=2023&nur_land=${bundeslandValue}`);
   const json = await response.json();
-
-  let feiertageString = JSON.stringify(json);
 
   createTable(json);
 
@@ -17,6 +13,8 @@ export async function getFeiertage() {
 
 function createTable(json: object) {
   let table = document.getElementById('table');
+  // @ts-ignore
+  table.innerHTML = "";
 
   let keys: string[] = [];
   Object.keys(json).forEach(prop => keys.push(prop));
@@ -30,13 +28,14 @@ function createTable(json: object) {
   const arr = [keys, dates];
 
   for (let i = 0; i < arr.length; i++) {
-    let row = document.createElement('tr');
+    let row = document.createElement('td');
     for (let j = 0; j < arr[i].length; j++) {
-      let column = document.createElement('td');
+      let column = document.createElement('tr');
       let content = document.createTextNode(arr[i][j]);
       column.appendChild(content);
       row.appendChild(column);
     }
+    // @ts-ignore
     table.appendChild(row);
   }
 
