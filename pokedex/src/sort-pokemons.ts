b8c8a17b0130af7875feace07a9b088a19a8a132
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2023-01-18 09:47:54 +0100

    fix typo

--- pokedex/src/sort-pokemons.ts
+++ pokedex/src/sort-pokemons.ts
@@ -1,7 +1,7 @@
 import { Pokemon } from './types/pokemon-api';
 
 /**
- * Pokemon werden in place anhand ihrer ID aufsteigend Sortiert
+ * Pokemon werden in place anhand ihrer ID aufsteigend sortiert
  */
 export function sortPokemons(pokemons: Pokemon[]): Pokemon[] {
   return pokemons.sort((a, b) => a.game_indices.id - b.game_indices.id);
