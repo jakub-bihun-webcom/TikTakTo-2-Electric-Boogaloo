c43bfeec32705f727762c46d524e6eea0db81955
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2023-01-25 14:39:12 +0100

    searchPokemon Component

--- pokedex-angular/my-app/src/app/app.module.ts
+++ pokedex-angular/my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchPokemonComponent
   ],
   imports: [
     BrowserModule,
---
+++ pokedex-angular/my-app/src/app/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-pokemon',
+  templateUrl: './search-pokemon.component.html',
+  styleUrls: ['./search-pokemon.component.css']
+})
+export class SearchPokemonComponent {
+  async searchPokemon() {
+    console.log('AYO HIER STEHT WAS WTF ?!?!?!');
+  }
+}
