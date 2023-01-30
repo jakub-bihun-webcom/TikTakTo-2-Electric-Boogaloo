f0abe77e1f3746984c0c275c81250d64f837ed5c
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-01-30 11:16:57 +0100

    Refactor

--- Feiertage-angular/src/app/feiertage.service.ts
+++ Feiertage-angular/src/app/feiertage.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { createEvents, DateArray } from 'ics';
-import { map, Observable, tap } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { FeiertagTableEntry } from './feiertage';
 
 @Injectable({
@@ -31,7 +31,6 @@ export class FeiertageService {
 
   getFeiertage(bundesland: string, year: string): Observable<FeiertagTableEntry[]> {
     return this.httpClient.get(`https://feiertage-api.de/api/?jahr=${year}&nur_land=${bundesland}`).pipe(
-      tap(value => console.log(value)),
       map(value => {
         const keys: string[] = Object.keys(value);
         const dates: string[] = Object.values(value).map(val => val.datum);
