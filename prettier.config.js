ec0e2f93f733efe13ec5173f77585dcc47cec715
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2023-02-02 17:30:03 +0100

    change prettier config back :D

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   arrowParens: 'avoid', // default: "es5"
-  printWidth: 140, // default: 80
+  printWidth: 120, // default: 80
   singleQuote: true, // default: false
   trailingComma: 'none', // default: "es5",
   bracketSameLine: true // default: false
