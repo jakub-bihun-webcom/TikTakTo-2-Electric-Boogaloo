6284fb11bd31c225a0f4ed0e28b086dd35ac0ec3
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2023-02-02 11:39:03 +0100

    remove console.log

--- pokedex-angular/src/app/services/apiRequests/pokemon-apirequest.service.ts
+++ pokedex-angular/src/app/services/apiRequests/pokemon-apirequest.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Pokemon } from '../../pokemon-api';
 
 @Injectable({
@@ -10,10 +10,6 @@ export class PokemonAPIRequestService {
   constructor(private httpClient: HttpClient) {}
 
   fetchPokemon(id: number): Observable<Pokemon> {
-    return this.httpClient.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`).pipe(
-      tap(value => {
-        console.log(value);
-      })
-    );
+    return this.httpClient.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`);
   }
 }
