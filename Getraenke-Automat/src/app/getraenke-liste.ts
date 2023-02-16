bd1c3ae4ef0a12d18676e06e8231dff104e2e712
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-02-16 12:29:17 +0100

    Add Getränkeliste

---
+++ Getraenke-Automat/src/app/getraenke-liste.ts
@@ -0,0 +1,7 @@
+export const getraenkeListe = [
+  { name: 'Cola', price: 2 },
+  { name: 'Fanta', price: 2.5 },
+  { name: 'Sprite', price: 1.5 },
+  { name: 'Wasser', price: 1 },
+  { name: 'Bier', price: 3 }
+];
