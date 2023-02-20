c90fcd91d0203529decb5c1d912894c6e54fd6d5
JoshyDerEchte <joshua.schlautmann@web-computing.de>
2023-02-20 09:52:16 +0100

    Working Login Screen

--- Sparkassenautomat/src/app/login-screen/login-screen.component.ts
+++ Sparkassenautomat/src/app/login-screen/login-screen.component.ts
@@ -8,8 +8,30 @@ import { Router } from '@angular/router';
 })
 export class LoginScreenComponent {
   constructor(private router: Router) {}
+  users = [
+    { id: '1234', pin: '5678' },
+    { id: '2345', pin: '2342' }
+  ];
+
+  pinInput?: String;
+  idInput?: String;
+  errorMessage: string = '';
 
   goToUserHomeScreen() {
-    this.router.navigate(['/user-home-screen']);
+    const user = this.users.find(u => u.id === this.idInput);
+    if (user && user.pin === this.pinInput) {
+      this.router.navigate(['/user-home-screen']);
+    } else {
+      this.displayError('Bitte überprüfen sie ihre anmelde Daten');
+      throw new Error('Input Validation ERROR')
+    }
+  }
+
+  private displayError(error: string) {
+    this.errorMessage = error;
+  }
+
+  clearError() {
+    this.errorMessage = '';
   }
 }
