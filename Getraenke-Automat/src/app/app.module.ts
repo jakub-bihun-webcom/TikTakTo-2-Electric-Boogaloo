0d80f6c5c49eca907de61e32f7b1ff00ed20e16d
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-02-21 15:59:02 +0100

    header css

--- Getraenke-Automat/src/app/app.module.ts
+++ Getraenke-Automat/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeverageListComponent } from './beverage-list/beverage-list.component';
+import { HeaderComponent } from './header/header.component';
 import { InputFieldComponent } from './input-field/input-field.component';
 import { OutputFieldComponent } from './output-field/output-field.component';
 import { CashRegistryComponent } from './cash-registry/cash-registry.component';
 import { OutputCostumerComponent } from './output-costumer/output-costumer.component';
 
 @NgModule({
-  declarations: [AppComponent, BeverageListComponent, InputFieldComponent, OutputFieldComponent, CashRegistryComponent, OutputCostumerComponent],
+  declarations: [AppComponent, BeverageListComponent, InputFieldComponent, OutputFieldComponent, CashRegistryComponent, OutputCostumerComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
