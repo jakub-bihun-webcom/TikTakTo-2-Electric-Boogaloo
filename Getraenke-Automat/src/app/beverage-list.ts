d0220fc393e62da59798bb699b96eb80867d5b17
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-02-27 12:55:10 +0100

    change beverageList

--- Getraenke-Automat/src/app/beverage-list.ts
+++ Getraenke-Automat/src/app/beverage-list.ts
@@ -1,9 +1,9 @@
 import { Beverage } from './beverage';
 
 export const beverageList: Beverage[] = [
-  { id: 1, name: 'Cola', price: 2, quantity: 8 },
-  { id: 2, name: 'Fanta', price: 2.5, quantity: 8 },
-  { id: 3, name: 'Sprite', price: 1.5, quantity: 8 },
-  { id: 4, name: 'Wasser', price: 1, quantity: 0 },
+  { id: 1, name: 'Cola', price: 2, quantity: 6 },
+  { id: 2, name: 'Fanta', price: 2.5, quantity: 4 },
+  { id: 3, name: 'Sprite', price: 1.5, quantity: 7 },
+  { id: 4, name: 'Wasser', price: 1, quantity: 3 },
   { id: 5, name: 'Bier', price: 3, quantity: 1 }
 ];
