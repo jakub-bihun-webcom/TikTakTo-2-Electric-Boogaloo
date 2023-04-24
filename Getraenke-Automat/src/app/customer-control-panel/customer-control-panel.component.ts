412c9c1e84fc24e1061bec8f8571fcdff63275a0
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-04-24 09:21:35 +0200

    Bugfix rounding error

--- Getraenke-Automat/src/app/customer-control-panel/customer-control-panel.component.ts
+++ Getraenke-Automat/src/app/customer-control-panel/customer-control-panel.component.ts
@@ -22,6 +22,7 @@ export class CustomerControlPanelComponent {
    */
   onMoneyPaid(money: number) {
     this.paidAmount += money;
+    this.paidAmount = (Math.round(this.paidAmount * 500)/ 500);
   }
 
   /**
