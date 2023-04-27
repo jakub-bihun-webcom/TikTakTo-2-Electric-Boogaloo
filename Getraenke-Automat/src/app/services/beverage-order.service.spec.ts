04956e203261e6c757658ae1c25c579969d6440f
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-04-27 17:21:34 +0200

    Add BeverageOrderService Test

--- Getraenke-Automat/src/app/services/beverage-order.service.spec.ts
+++ Getraenke-Automat/src/app/services/beverage-order.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { BeverageOrderService } from './beverage-order.service';
 
-describe('BeverageOrderService', () => {
+fdescribe('BeverageOrderService', () => {
   let service: BeverageOrderService;
 
   beforeEach(() => {
@@ -10,7 +10,23 @@ describe('BeverageOrderService', () => {
     service = TestBed.inject(BeverageOrderService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should get Beverage Price', () => {
+    expect(service.getBeveragePrice(1)).toEqual(2)
+    expect(service.getBeveragePrice(2)).toEqual(2.5)
+    expect(service.getBeveragePrice(3)).toEqual(1.5)
+    expect(service.getBeveragePrice(4)).toEqual(1)
+  });
+
+  it('should get Beverage Name', () => {
+    expect(service.getBeverageName(1)).toEqual('Cola')
+    expect(service.getBeverageName(2)).toEqual('Fanta')
+    expect(service.getBeverageName(5)).toEqual('Bier')
+  });
+
+  it('should check for Beverage availability', () => {
+    expect(service.checkAvailability(5)).toBeTruthy()
+
+    expect(service.getBeverageName(2)).toEqual('Fanta')
+    expect(service.getBeverageName(5)).toEqual('Bier')
   });
 });
