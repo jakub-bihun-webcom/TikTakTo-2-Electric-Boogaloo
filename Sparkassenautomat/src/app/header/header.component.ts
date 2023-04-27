a0a88fff0c3457d693eb200a13746a22e6e6cd28
Joshua <joshua.schlautmann@web-computing.de>
2023-04-27 16:28:32 +0200

    Got rid of unused Login Button

--- Sparkassenautomat/src/app/header/header.component.ts
+++ Sparkassenautomat/src/app/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +6,4 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  constructor(private router: Router) {}
 }
