74c940e0134dc66bb0ae4178a3740fb18254b4e2
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-04-27 13:32:50 +0200

    Create Tests for handleOrder Service

--- Getraenke-Automat/src/app/services/handle-order.service.spec.ts
+++ Getraenke-Automat/src/app/services/handle-order.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { HandleOrderService } from './handle-order.service';
 
-describe('HandleOrderService', () => {
+fdescribe('HandleOrderService', () => {
   let service: HandleOrderService;
 
   beforeEach(() => {
@@ -11,6 +11,10 @@ describe('HandleOrderService', () => {
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service.isOrderValid(1, 'wrongID')).toBeFalsy();
+    expect(service.isOrderValid(3, 'a')).toBeFalsy();
+    expect(service.isOrderValid(5, '5')).toBeTruthy();
+    expect(service.isOrderValid(4, '4')).toBeTruthy();
+    expect(service.isOrderValid(3, '2')).toBeTruthy();
   });
 });
