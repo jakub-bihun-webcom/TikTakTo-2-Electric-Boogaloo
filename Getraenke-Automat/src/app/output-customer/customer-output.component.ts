2cd9a44b1364255a4a8e2781a7a136379dc46358
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-05-03 12:49:02 +0200

    Change to have one place for CustomerOutput

--- Getraenke-Automat/src/app/output-customer/customer-output.component.ts
+++ Getraenke-Automat/src/app/output-customer/customer-output.component.ts
@@ -14,61 +14,17 @@ import { CustomerMessageService } from '../services/customer-message.service';
  * oder der Anforderung des Rückgelds wird die Ausgabe in dieser Komponente simuliert.
  */
 export class CustomerOutputComponent {
-  /**
-   * Repräsentiert das Ausgabefach für Getränke
-   */
-  beverageName?: string;
-
-  /**
-   * Repräsentiert das Rückgeld
-   */
-  change?: number;
 
   constructor(
-    private beverageOutputService: BeverageOutputService,
+    protected beverageOutputService: BeverageOutputService,
     private customerMessageService: CustomerMessageService,
     private cashRegisterService: CashRegisterService
   ) {}
-
-  ngOnInit(): void {
-    this.beverageOutputService.orderOutput.subscribe(order => {
-      this.updateOutputCompartments(order.beverageName, order.change);
-    });
-    this.beverageOutputService.canceledMoney.subscribe((value: number) => {
-      this.addChange(value);
-    });
-  }
-
-  /**
-   * Aktualisiert den Stand des Ausgabefaches und des Rückgeldes.
-   */
-  updateOutputCompartments(beverageName: string, change: number) {
-    this.addChange(change);
-    if (this.beverageName === undefined) {
-      this.beverageName = beverageName;
-    } else {
-      this.beverageName = beverageName + ' ' + this.beverageName;
-    }
-  }
-
-  /**
-   * Addiert change dem Rückgeld hinzu.
-   */
-  addChange(change: number) {
-    if (this.change === undefined) {
-      this.change = change;
-    } else {
-      this.change += change;
-      this.change = this.cashRegisterService.roundMoneyToFiveCents(this.change);
-    }
-  }
 
   /**
    * Setzt das Ausgabefach für Getränke, das Rückgeld und die customerMessage auf ihre Ursprungswerte zurück.
    */
   resetOutput() {
-    this.change = 0;
-    this.beverageName = '';
     this.beverageOutputService.resetOrderOutputState();
     this.customerMessageService.setCustomerMessage('Willkommen beim besten Getränke Automaten');
   }
--- Getraenke-Automat/src/app/services/beverage-output.service.ts
+++ Getraenke-Automat/src/app/services/beverage-output.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CustomerOutputComponent } from '../output-customer/customer-output.component';
 import { BeverageOrderService } from './beverage-order.service';
 import { BehaviorSubject } from 'rxjs';
 
@@ -12,22 +13,27 @@ export class BeverageOutputService {
   });
   canceledMoney = new BehaviorSubject<number>(0);
 
-  constructor(private beverageOrderService: BeverageOrderService) {}
+  constructor(
+    private beverageOrderService: BeverageOrderService,
+  ) {}
 
   setOrder(change: number, beverageID: number) {
     const name = this.beverageOrderService.getBeverageName(beverageID);
     this.setOrderOutput(name, change);
   }
 
-  private setOrderOutput(name: string, change: number) {
+  setOrderOutput(name: string, change: number) {
     this.orderOutput.next({
-      change: change,
-      beverageName: name
+      change: change + this.orderOutput.getValue().change,
+      beverageName: this.orderOutput.getValue().beverageName + ' ' + name
     });
   }
 
   returnMoney(value: number) {
-    this.canceledMoney.next(value);
+    this.orderOutput.next({
+      change: value,
+      beverageName: this.orderOutput.getValue().beverageName
+    })
   }
 
   resetOrderOutputState() {
