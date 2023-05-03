be9e144de239a645e800912d44197fa0376259e0
Joshua <joshua.schlautmann@web-computing.de>
2023-05-03 12:15:17 +0200

    Add comments

--- Sparkassenautomat/src/app/user-cashout/services/user-cash-out-manager.service.ts
+++ Sparkassenautomat/src/app/user-cashout/services/user-cash-out-manager.service.ts
@@ -7,6 +7,13 @@ export class UserCashOutManager {
   private ATMAccountMoney: number = 10000;
   private userAccountMoney?: number;
 
+
+  /**
+   * Zieht einen bestimmten Betrag vom Benutzerkonto ab.
+   * @param amount Der abzuziehende Betrag
+   * @returns Den aktualisierten Kontostand des Benutzers
+   * @throws Error, wenn der Kontostand des Benutzers nicht gesetzt ist
+   */
   subtractUserAccountMoney(amount: number) {
     if (this.userAccountMoney === undefined) {
       throw new Error('User account money not set');
@@ -15,10 +22,18 @@ export class UserCashOutManager {
     return this.userAccountMoney;
   }
 
+  /**
+   * Setzt den Kontostand des Benutzers.
+   * @param amount Der zu setzende Kontostand.
+   */
   setUserAccountMoney(amount: number) {
     this.userAccountMoney = amount;
   }
 
+/**
+ * Verarbeitet den Betrag auf dem ATM-Konto.
+ * @param amount Der zu verarbeitende Betrag.
+ */
   processATMAccountMoney(amount: number) {
     if (this.ATMAccountMoney + 1 <= amount) {
       return [0, this.ATMAccountMoney];
