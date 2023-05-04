6eb958f4aa7efa180cee6d6405354e288198d754
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-05-04 08:58:00 +0200

    Delete unused tests

--- Getraenke-Automat/src/app/services/beverage-output.service.spec.ts
+++ Getraenke-Automat/src/app/services/beverage-output.service.spec.ts
@@ -1,16 +1,1 @@
-import { TestBed } from '@angular/core/testing';
 
-import { BeverageOutputService } from './beverage-output.service';
-
-describe('BeverageOutputService', () => {
-  let service: BeverageOutputService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BeverageOutputService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
