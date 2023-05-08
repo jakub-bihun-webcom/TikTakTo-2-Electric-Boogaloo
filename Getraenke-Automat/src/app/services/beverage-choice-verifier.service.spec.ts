582fbe54825ceda7ce58fceca20cf44943c570eb
Niklas Kiefer <niklas.kiefer@web-computing.de>
2023-05-08 14:20:47 +0200

    Update tests and fix bug

--- Getraenke-Automat/src/app/services/beverage-choice-verifier.service.spec.ts
+++ Getraenke-Automat/src/app/services/beverage-choice-verifier.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { BeverageChoiceVerifierService } from './beverage-choice-verifier.service';
 
-describe('VerifyInputService', () => {
+fdescribe('VerifyInputService', () => {
   let service: BeverageChoiceVerifierService;
 
   beforeEach(() => {
@@ -10,10 +10,19 @@ describe('VerifyInputService', () => {
     service = TestBed.inject(BeverageChoiceVerifierService);
   });
 
-  it('should check if valid BeverageCompartment ID', () => {
+  it('should pass verification for valid compartment ID', () => {
     expect(service.isValidID('1')).toBeTruthy();
+    expect(service.isValidID('2')).toBeTruthy();
     expect(service.isValidID('5')).toBeTruthy();
+  });
+
+  it('should fail validation for not existing compartment ID', () => {
+    expect(service.isValidID('0')).toBeFalsy();
+    expect(service.isValidID('6')).toBeFalsy();
     expect(service.isValidID('12')).toBeFalsy();
+  });
+
+  it('should fail validation for non-number like inputs', () => {
     expect(service.isValidID('wrongID')).toBeFalsy();
     expect(service.isValidID('string')).toBeFalsy();
   });
